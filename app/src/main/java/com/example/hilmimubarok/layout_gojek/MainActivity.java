3ce0912ad223fe6dd93ba70c18f9d001bc6a4a71
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2017-08-11 22:13:11 +0700

    Layout Gojek

---
+++ app/src/main/java/com/example/hilmimubarok/layout_gojek/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hilmimubarok.layout_gojek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
